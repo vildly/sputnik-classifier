7c31dade23d469785260975c4ad1cabcb2dc0e22
Sam <samme.s90@gmail.com>
2025-02-18 15:04:22 +0100

    add shell for component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import InputBox from "./components/InputBox"
+
  export default function App() {
     return (
         <div className="flex justify-center">
-          <h1 className="text-white text-2xl">hello world</h1> 
+            <InputBox />
         </div>
     )
 }
---
+++ client/src/components/InputBox.tsx
@@ -0,0 +1,7 @@
+export default function InputBox() {
+    return (
+        <div>
+            <h1 className="text-white text-2xl">hello world</h1>
+        </div>
+    )
+}
