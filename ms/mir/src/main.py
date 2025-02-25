52d48f6f0c0499b42cb3d8d85ab36d83b2447aef
Sam <samme.s90@gmail.com>
2025-02-25 17:49:00 +0100

    remove kafka

--- ms/mir/src/main.py
+++ ms/mir/src/main.py
@@ -1,30 +1,8 @@
 import os
-import json
 import nltk
 from dotenv import load_dotenv
-from kafka import KafkaProducer, producer
 
-from clean import filter_data
-from db import add_one, connect_to_database, find_one, get_collection
-
-
-def create_producer() -> KafkaProducer:
-    # Instantiate a KafkaProducer that connects to the Docker Kafka broker on 0.0.0.0:9002.
-    # The value_serializer function converts a Python object to JSON-encoded bytes.
-    producer = KafkaProducer(
-        bootstrap_servers=["0.0.0.0:9002"],
-        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-    )
-    return producer
-
-
-def send_message(producer: KafkaProducer, topic, message):
-    # Send a message to the specified topic and wait for confirmation.
-    future = producer.send(topic, message)
-    result = future.get(timeout=10)
-    print(
-        f"Message sent to topic {result.topic} in partition {result.partition} with offset {result.offset}"
-    )
+from db import connect_to_database
 
 
 def get_dependencies() -> None:
@@ -46,15 +24,6 @@ def get_dependencies() -> None:
 if __name__ == "__main__":
     load_dotenv()
     connect_to_database(os.getenv("MONGODB_URI"))
-
-    topic = "events"
-    message = {"example": "Kafka message from python producer"}
-
-    producer = create_producer()
-    send_message(producer, topic, message)
-
-    # Ensure all buffered messages are sent
-    producer.flush()
 
     # col = get_collection("db", "preprocessed")
     # print(add_one(col=col, data={"data": "hello world"}))
