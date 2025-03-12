5a4706b5fcd99f398d5060188aa549c7e4051753
Sam <samme.s90@gmail.com>
2025-03-12 10:37:47 +0100

    use env for nltk data

--- src/mir/clean.py
+++ src/mir/clean.py
@@ -12,7 +12,7 @@ from nltk.tokenize import RegexpTokenizer, word_tokenize
 # -----------------------------------------------------------------------------
 # NLTK Dependency Download
 # -----------------------------------------------------------------------------
-download_dir = "./.venv/lib/nltk_data"
+download_dir = os.getenv("NLTK_DATA")
 download(info_or_id="stopwords", download_dir=download_dir)
 download(info_or_id="punkt", download_dir=download_dir)
 download(info_or_id="punkt_tab", download_dir=download_dir)
